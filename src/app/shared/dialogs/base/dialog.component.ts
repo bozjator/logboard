64033ee0b2fded9687109af2b237cbb38365efad
Blaz Bozja <bozjator@gmail.com>
2025-02-17 20:20:56 +0100

    feat: dialog component

---
+++ src/app/shared/dialogs/base/dialog.component.ts
@@ -0,0 +1,58 @@
+import { Component, input, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-dialog',
+  imports: [CommonModule],
+  template: `
+    <div class="fixed inset-0 z-40 bg-black/40">
+      <div class="absolute inset-0"></div>
+
+      <div
+        class="relative z-50 m-28 mx-auto flex max-h-[90vh] w-auto max-w-fit flex-col rounded-lg bg-zinc-100 dark:bg-zinc-500"
+      >
+        <!-- Header Section (Sticky Top) -->
+        <div class="flex-shrink-0 p-4 pb-2">
+          <div class="relative">
+            <div class="absolute inset-0 flex items-center">
+              <div
+                class="w-full border-t border-gray-600 dark:border-gray-200"
+              ></div>
+            </div>
+            <div class="relative flex justify-start">
+              <span
+                class="bg-zinc-100 pr-3 text-base leading-6 font-semibold text-gray-600 dark:bg-zinc-500 dark:text-gray-200"
+              >
+                {{ title() }}
+              </span>
+            </div>
+          </div>
+          <ng-content select="[sticky-top]"></ng-content>
+        </div>
+
+        <!-- Scrollable Content Section -->
+        <div class="flex-grow overflow-x-hidden overflow-y-auto p-4 pt-2">
+          <ng-content></ng-content>
+        </div>
+
+        <!-- Footer Section (Sticky Bottom) -->
+        <div
+          class="flex-shrink-0 rounded-b-lg bg-zinc-200 p-2 dark:bg-zinc-400"
+        >
+          <ng-content select="[sticky-bottom]"></ng-content>
+          <div class="flex items-center justify-between">
+            <div class="flex space-x-3">
+              <ng-content select="[footer-start]"></ng-content>
+            </div>
+            <div class="flex space-x-3">
+              <ng-content select="[footer]"></ng-content>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  `,
+})
+export class DialogComponent {
+  title = input('');
+}
---
+++ src/app/shared/dialogs/base/dialog.service.ts
@@ -0,0 +1,68 @@
+import {
+  Injectable,
+  ComponentRef,
+  createComponent,
+  ApplicationRef,
+  EnvironmentInjector,
+  inject,
+} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DialogService {
+  private appRef = inject(ApplicationRef);
+  private environmentInjector = inject(EnvironmentInjector);
+
+  private containerElement: HTMLElement | null = null;
+
+  private ensureContainer() {
+    if (!this.containerElement) {
+      this.containerElement = document.createElement('div');
+      document.body.appendChild(this.containerElement);
+    }
+  }
+
+  open<T>(component: any, data?: T) {
+    this.ensureContainer();
+    if (!this.containerElement) return;
+
+    // Dynamically create the dialog component.
+    const componentRef = createComponent(component, {
+      environmentInjector: this.environmentInjector,
+    });
+    const dialogInstance = componentRef.instance as any;
+
+    // Pass data to the dialog component.
+    if (data && dialogInstance.dialogData)
+      try {
+        dialogInstance.dialogData.set(data);
+      } catch (error) {
+        console.error('Failed to set dialog data: ', error);
+      }
+
+    // Close dialog signal listener.
+    if (dialogInstance.dialogClose)
+      dialogInstance.dialogClose.subscribe(() => this.close(componentRef));
+
+    // Attach the component to the application view.
+    this.appRef.attachView(componentRef.hostView);
+
+    // Add the dialog to the container element.
+    const dialogElement = componentRef.location.nativeElement;
+    this.containerElement.appendChild(dialogElement);
+
+    return componentRef;
+  }
+
+  private close(componentRef: ComponentRef<any>) {
+    this.appRef.detachView(componentRef.hostView);
+    componentRef.destroy();
+
+    // Remove container if no dialogs are left.
+    if (this.containerElement?.childElementCount === 0) {
+      this.containerElement.remove();
+      this.containerElement = null;
+    }
+  }
+}
