81c76426aa237e40b2811d0bcc77366f822ed929
Blaz Bozja <bozjator@gmail.com>
2025-02-21 20:06:00 +0100

    feat: monitoring general logs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainToolbarComponent } from './features/main-toolbar.component';
 import { MonitoringLogsComponent } from './features/monitoring-logs/monitoring-logs.component';
+import { MonitoringGeneralComponent } from './features/monitoring-general/monitoring-general.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MainToolbarComponent, MonitoringLogsComponent],
+  imports: [
+    RouterOutlet,
+    MainToolbarComponent,
+    MonitoringLogsComponent,
+    MonitoringGeneralComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/features/monitoring-general/monitoring-general.component.ts
@@ -0,0 +1,91 @@
+import { Component, effect, inject, signal } from '@angular/core';
+import { ApiMonitoringService } from '../../shared/services/api-monitoring.service';
+import { MonitoringEndpointService } from '../../shared/services/monitoring-endpoint.service';
+import { LogsTableComponent } from '../monitoring-logs/components/logs-table/logs-table.component';
+import { TitleRightLineComponent } from '../../shared/components/title-right-line.component';
+import { ObjectLogsTableComponent } from '../../shared/components/object-table/object-table.component';
+import { MonitoringLoggerDBTransportError } from '../../shared/models/monitoring-logger-db-transport-error.dto';
+import { CamelCaseToTitlePipe } from '../../shared/pipes/camel-case-to-title.pipe';
+
+@Component({
+  selector: 'app-monitoring-general',
+  imports: [
+    LogsTableComponent,
+    TitleRightLineComponent,
+    ObjectLogsTableComponent,
+    CamelCaseToTitlePipe,
+  ],
+  template: `
+    <div class="text-black dark:text-white">
+      <app-title-right-line
+        [title]="'Logger database transport error logs'"
+        [textColor]="'text-gray-700 dark:text-gray-300'"
+        [borderColor]="'border-gray-400'"
+        [bgColor]="'bg-neutral-100 dark:bg-zinc-800'"
+      />
+      @for (log of loggerDBTransportErrorLogs(); track $index) {
+        <app-title-right-line
+          [class]="'text-xs'"
+          [title]="'Error and logger log'"
+          [textColor]="'text-gray-700 dark:text-gray-300'"
+          [borderColor]="'border-gray-400'"
+          [bgColor]="'bg-neutral-100 dark:bg-zinc-800'"
+        />
+        <app-object-table [objects]="[log.error]" />
+        <app-logs-table
+          [logs]="{ totalCount: 1, result: [log.loggerRecord] }"
+        />
+        <div class="mx-2 my-6 border-t border-gray-400"></div>
+      }
+
+      @for (item of otherData(); track $index) {
+        <app-title-right-line
+          [class]="'text-md'"
+          [title]="item[0] | camelCaseToTitle"
+          [textColor]="'text-gray-700 dark:text-gray-300'"
+          [borderColor]="'border-gray-400'"
+          [bgColor]="'bg-neutral-100 dark:bg-zinc-800'"
+        />
+        @if (typeof item[1][0] === 'object') {
+          <app-object-table [objects]="item[1]" />
+        } @else {
+          <div class="ml-6">
+            {{ JSON.stringify(item[1]) }}
+          </div>
+        }
+      }
+    </div>
+  `,
+})
+export class MonitoringGeneralComponent {
+  private apiMonitoringService = inject(ApiMonitoringService);
+  private monitoringEndpointService = inject(MonitoringEndpointService);
+
+  JSON = JSON;
+  loggerDBTransportErrorLogs = signal<MonitoringLoggerDBTransportError[]>([]);
+  otherData = signal<(string[] | [string, any])[]>([]);
+
+  constructor() {
+    this.onEndpointChange();
+  }
+
+  private onEndpointChange() {
+    effect(() => {
+      const currentEndpoint = this.monitoringEndpointService.currentEndpoint();
+      if (!currentEndpoint) return;
+      this.getGeneralData();
+    });
+  }
+
+  private getGeneralData() {
+    this.apiMonitoringService.getGeneral().subscribe({
+      next: (infoData) => {
+        this.loggerDBTransportErrorLogs.set(
+          infoData.loggerDBTransportErrorLogs,
+        );
+        const { loggerDBTransportErrorLogs, ...otherData } = infoData;
+        this.otherData.set(Object.entries(otherData));
+      },
+    });
+  }
+}
