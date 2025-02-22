8c881f9837579fb9b4c6708af7695049ea0e006a
Blaz Bozja <bozjator@gmail.com>
2025-02-22 11:16:18 +0100

    fix: show delete current endpoint only if edit mode

--- src/app/shared/dialogs/monitoring-endpoint.dialog.ts
+++ src/app/shared/dialogs/monitoring-endpoint.dialog.ts
@@ -89,23 +89,25 @@ interface MonitoringEndpointForm {
           />
         </form>
       </div>
-      <ng-container footer-start>
-        <button
-          (click)="openDialogDeleteEndpoint()"
-          class="cursor-pointer rounded-md bg-red-500 px-2.5 py-1.5 text-sm font-semibold text-white shadow-xs hover:bg-red-500"
-        >
-          <svg
-            height="24px"
-            viewBox="0 -960 960 960"
-            width="24px"
-            fill="#e8eaed"
+      @if (dialogData().isModeEditCurrent) {
+        <ng-container footer-start>
+          <button
+            (click)="openDialogDeleteEndpoint()"
+            class="cursor-pointer rounded-md bg-red-500 px-2.5 py-1.5 text-sm font-semibold text-white shadow-xs hover:bg-red-500"
           >
-            <path
-              d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"
-            />
-          </svg>
-        </button>
-      </ng-container>
+            <svg
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#e8eaed"
+            >
+              <path
+                d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"
+              />
+            </svg>
+          </button>
+        </ng-container>
+      }
       <ng-container footer>
         @if (errorMessage()) {
           <div class="flex items-center text-red-700">{{ errorMessage() }}</div>
